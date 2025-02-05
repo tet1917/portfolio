bee9b397cb9ecf19e4aa9fe721d26990572d3bcc
take1917 <tetsu0224.soccer@icloud.com>
2025-02-05 22:26:06 +0900

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Montserrat , Zen_Kaku_Gothic_New } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
+const geistSans = Montserrat({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
+const geistMono = Zen_Kaku_Gothic_New({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+  weight:"400",
 });
 
 export const metadata: Metadata = {
