69bb7b5b91a19149b5a5f1781482a9f2e6284e00
kasumi0 <parfumdelapeche@gmail.com>
2025-02-21 22:57:01 +0900

    parallax/scroll-driven-animation/common-h2/common-button

---
+++ app/features/Top/component/parallax/Parallax.tsx
@@ -0,0 +1,32 @@
+import { ParallaxImage } from "./ParallaxImage";
+import styles from "./parallax.module.css";
+
+const images = [
+  { src: "/about/1.jpg", speed: 3 },
+  { src: "/about/2.jpg", speed: 2 },
+  { src: "/about/3.jpg", speed: 1.5 },
+  { src: "/about/4.jpg", speed: 2.5 },
+  { src: "/about/5.jpg", speed: 1 },
+  { src: "/about/6.jpg", speed: 1 },
+  { src: "/about/7.jpg", speed: 1 },
+  { src: "/about/8.jpg", speed: 0.8 },
+];
+
+export const Parallax = () => {
+  return (
+    <div className={styles.parallax}>
+      {images.map((image, i) => (
+        <ParallaxImage key={i} {...image} />
+      ))}
+      <h3>
+        <span>Design</span>
+        {/* <span>Produce</span>
+        <span>Make</span>
+        <span>Develop</span> */}
+        <span>Innovation</span>
+        {/* <span>Be</span>
+        <span>Innovative</span> */}
+      </h3>
+    </div>
+  );
+};
--- app/features/Top/page.tsx
+++ app/features/Top/page.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { FaXTwitter, FaInstagram, FaGithub } from "react-icons/fa6";
 import styles from "./top.module.css";
-import { Name } from "./component/Name";
+import { Name } from "./component/name/Name";
+import { Parallax } from "./component/parallax/Parallax";
+import { Heading } from "../../component/heading/Heading";
+import { MoreButton } from "../../component/buttons/more/MoreButton";
 
 const Top = () => {
   return (
@@ -17,9 +20,16 @@ const Top = () => {
           <FaGithub />
         </a>
       </div>
+
       <section className={styles.hero}>
         <p className={styles.scroll}>Scroll</p>
         <Name text="Tetsu Kazui" />
+      </section>
+
+      <section className={styles.about}>
+        <Heading ja={"僕について"} en={"about"}></Heading>
+        <Parallax />
+        <MoreButton href={'/about'}></MoreButton>
       </section>
     </>
   );
